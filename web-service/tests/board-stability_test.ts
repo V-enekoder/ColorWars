e378ec0bc5a8a9d8c16d29eda6cfa890ed7d298c
Victor Astudillo <vjxoen@gmail.com>
2026-04-14 22:33:46 -0400

    test(web-service): add board stability test
    assuring no cell have critical points after a reaction

---
+++ web-service/tests/board-stability_test.ts
@@ -0,0 +1,18 @@
+import { assertEquals } from "https://deno.land/std/assert/mod.ts";
+import { GameEngine } from "../core/GameLogic.ts";
+import { RulesOptions } from "../utils/types.ts";
+
+Deno.test("board stability: no cell with critical points", () => {
+  const engine = new GameEngine(5, 5, 3, 2, RulesOptions.EmptyAndOwnOrbs);
+  for (let i = 0; i < 50; i++) {
+    const moves = engine.getLegalMoves(engine.currentPlayerId);
+    if (moves.length === 0) break;
+    const move = moves[0];
+    const gen = engine.playGenerator(engine.getIndex(move.row, move.col));
+    while (!gen.next().done);
+
+    engine.getBoard().forEach((cell, idx) => {
+      assertEquals(cell.points < 3, true, `Cell ${idx} pass the limit`);
+    });
+  }
+});
